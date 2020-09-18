f414765e9cfef359c47e86c165740d9f6e9a73bd
Nicholas <69174708+berubenic@users.noreply.github.com>
2020-09-18 18:13:38 -0400

    works up to verify player guess

--- lib/game.rb
+++ lib/game.rb
@@ -3,6 +3,19 @@ class Game
 
   def initialize(secret_pattern)
     @secret_pattern = secret_pattern
-    p @secret_pattern
+    start_game
+  end
+
+  def start_game
+    puts 'The codemaker has created a secret pattern'
+    puts 'Guess four colors from these choices: '
+    puts 'blue, green, yellow, red, pink, orange'
+    ask_player_guess
+  end
+
+  def ask_player_guess
+    puts 'Make a guess'
+    player_guess = gets.chomp
+    verify_turn(player_guess)
   end
 end
